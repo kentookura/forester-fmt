01d3ff98ba9a376ac67951f45f7236fa8342fc45
Kento Okura <kentookura@gmail.com>
2024-01-11 23:53:42 +0100

    more coverage

--- bin/main.ml
+++ bin/main.ml
@@ -1,67 +1,100 @@
-open Pretty_expressive
 open Forester
+open Pretty_expressive
 open Core
 open Lexing
+open Query
 module Terminal = Asai.Tty.Make (Reporter.Message)
 
 let format_code (doc : Code.t) (w : int) =
   let cf = Printer.default_cost_factory ~page_width:w () in
   let module P = Printer.Make ((val cf)) in
   let open P in
-  let bs c = "\\" ^ c in
-  let braces c = "{" ^ c ^ "}" in
-  let textcmd label arg = P.text @@ bs @@ label ^ braces arg in
-  let rec pretty_node (node : Code.node Range.located) =
-    match node.value with
+  let bs c = text "\\" ^^ c in
+  let braces c = text "{" ^^ c ^^ text "}" in
+  let textcmd label arg = bs @@ text label ^^ braces (text arg) in
+  let query q =
+    let t =
+      match q with
+      | Author a | Tag a | Taxon a -> text "/author" ^^ text a
+      | Meta (_s, a) -> text "/meta" ^^ text a
+      | Or a -> text "/or" ^^ hcat @@ List.map (fun _str_t -> text "foo") a
+      | And a -> text "/and" ^^ hcat @@ List.map (fun _str_t -> text "foo") a
+      | Not _a -> text "/not" ^^ text "todo"
+      | True -> text "\\true"
+    in
+    text "\\query/" ^^ t
+  in
+  let _queries = List.map query in
+
+  let cmd t n = text "\\" ^^ text t ^^ text "{" ^^ n ^^ text "}" in
+  let delim d =
+    let f (a, b) = (text a, text b) in
+    match d with
+    | Squares -> f ("[", "]")
+    | Braces -> f ("{", "}")
+    | Parens -> f ("(", ")")
+  in
+  let rec pretty_node (node : Code.node) =
+    match node with
     | Text s -> P.text s
-    | Ident (_, _) -> text "\\todo{todo}"
-    | Group _ -> P.text "todo"
+    | Ident (l, n) -> bs @@ hcat (List.map text l) ^^ hcat (List.map text n)
+    | Group (d, m) ->
+        let f (a, b) m = hcat [ a; m; b ] in
+        f (delim d) (pretty m)
     (* | Xml_tag (r, s, t) -> P.text "\\xml{}" *)
-    | Xml_tag _ -> P.text "\\xml{}"
+    | Xml_tag (ls, _, n) -> P.text "\\xml" ^^ text ls ^^ pretty n ^^ nl
     | Transclude addr -> textcmd "transclude" addr
     | Embed_tex _ -> P.text "todo"
-    | Let _ -> P.text "todo"
+    | Let _ -> P.text "\\let"
     | Prim (t, n) -> (
         match t with
-        | `P -> text "\\p{" ^^ pretty n ^^ text "}"
-        | `Ol -> text "\\ol{" ^^ pretty n ^^ text "}"
-        | `Ul -> text "\\ul{" ^^ pretty n ^^ text "}"
-        | `Li -> text "\\li{" ^^ pretty n ^^ text "}"
-        | `Em -> text "\\em{" ^^ pretty n ^^ text "}"
-        | `Strong -> text "\\strong{" ^^ pretty n ^^ text "}"
-        | `Code -> text "\\code{" ^^ pretty n ^^ text "}"
-        | `Blockquote -> text "\\blockquote{" ^^ pretty n ^^ text "}"
-        | `Pre -> text "\\pre{" ^^ pretty n ^^ text "}")
-    | Object _ -> P.text "todo"
-    | Patch _ -> P.text "todo"
-    | Call _ -> P.text "todo"
-    | Query _ -> P.text "todo"
-    | Def _ -> P.text "todo"
-    | Alloc _ -> P.text "todo"
-    | Title a -> text "\\author{" ^^ pretty a ^^ text "}"
-    | Meta _ -> P.text "todo"
-    | Author a -> textcmd "author" a
-    | Tag t -> textcmd "tag" t
-    | TeX_package _ -> P.text "todo"
-    | Date d -> textcmd "date" d
-    | Namespace _ -> P.text "todo"
-    | Math (mode, _) -> (
+        | `P -> cmd "p" @@ pretty n
+        | `Ol -> cmd "ol" @@ pretty n
+        | `Ul -> cmd "ul" @@ pretty n
+        | `Li -> cmd "li" @@ pretty n
+        | `Em -> cmd "em" @@ pretty n
+        | `Strong -> cmd "strong" (pretty n)
+        | `Code -> cmd "code" (pretty n)
+        | `Blockquote -> cmd "blockquote" (pretty n)
+        | `Pre -> cmd "pre" (pretty n))
+    (* | Object { self = s; methods = ms } -> obj s ms *)
+    | Object _ -> text "\\object"
+    | Patch _ -> P.text "patch"
+    | Call _ -> P.text "call"
+    | Query _ -> P.text "\\query"
+    | Def (_, _, n) -> nl ^^ text "\\def" ^^ pretty n ^^ nl
+    | Alloc _ -> P.text "\\alloc"
+    | Title a -> text "\\title{" ^^ pretty a ^^ text "}"
+    | Meta (_, _) -> P.text "\\meta" ^^ nl
+    | Author a -> textcmd "author" a ^^ nl
+    | Tag t -> textcmd "tag" t ^^ nl
+    | TeX_package _ -> P.text "\\texpackage"
+    | Date d -> textcmd "date" d ^^ nl
+    | Namespace _ -> P.text "\\namespace"
+    | Math (mode, n) -> (
         match mode with
-        | Inline -> P.text "#{todo}"
-        | Display -> P.text "##{todo}")
+        | Inline -> P.text "#{ " ^^ pretty n ^^ text "}"
+        | Display -> P.text "##{" ^^ pretty n ^^ text "}")
     | Open _ -> P.text "todo"
-    | Scope _ -> P.text "todo"
-    | Put _ -> P.text "todo"
+    | Scope ns -> nl ^^ text "\\scope{" ^^ pretty ns ^^ text "}" ^^ nl
+    | Put (l, t) ->
+        P.text "\\put"
+        ^^ (hcat @@ List.map (fun c -> text "\\" ^^ text c) l)
+        ^^ pretty t
     | Default _ -> P.text "todo"
     | Get _ -> P.text "todo"
     | If_tex _ -> P.text "todo"
-    | Import (_, addr) -> textcmd "import" addr
-    | Taxon t -> textcmd "taxon" t
+    | Import (vis, addr) -> (
+        match vis with
+        | Private -> textcmd "import" addr ^^ nl
+        | Public -> textcmd "export" addr ^^ nl)
+    | Taxon t -> textcmd "taxon" t ^^ nl
     | Block _ -> P.text "todo"
   and pretty d =
     match d with
     | [] -> P.text ""
-    | node :: nodes -> pretty_node node <+> pretty nodes
+    (* TODO: check which constructs are fine to reorder. Frontmatter only?  *)
+    | node :: nodes -> pretty_node node.value <+> pretty nodes
   in
   P.pretty_print @@ pretty doc
 
@@ -80,7 +113,7 @@ let load_file filepath =
         Reporter.fatalf ~loc Parse_error "unrecognized token `%s`"
         @@ String.escaped token
   in
-  Format.printf "%s" (format_code code 80)
+  Format.printf "\n%s" (format_code code 80)
 
 let () =
   let display d = Terminal.display d in
