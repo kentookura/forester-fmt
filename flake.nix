b19d7d8b158550a39ceb8c8badf861c8fe8f3e60
Kento Okura <kentookura@gmail.com>
2024-02-08 15:17:59 +0000

    udpate flake

--- flake.nix
+++ flake.nix
@@ -1,23 +1,45 @@
 {
-  description = "A flake demonstrating how to build OCaml projects with Dune";
-
-  inputs = { flake-utils.url = "github:numtide/flake-utils"; };
-
-  outputs = { self, nixpkgs, flake-utils }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in flake-utils.lib.eachDefaultSystem (system: {
-      devShells = {
-        default = pkgs.mkShell {
-          packages = with pkgs.ocaml-ng.ocamlPackages_5_0;
-            with pkgs; [
-              dune_3
-              opam
-              ocaml
-            ];
-          shellHook = "eval $(opam env --switch=5.1.0)";
+  inputs = {
+    opam-nix.url = "github:tweag/opam-nix";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.follows = "opam-nix/nixpkgs";
+  };
+  outputs = { self, flake-utils, opam-nix, nixpkgs }@inputs:
+    let package = "forester_format";
+    in flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        on = opam-nix.lib.${system};
+        devPackagesQuery = {
+          ocaml-base-compiler = "5.1.1";
+          ocaml-lsp-server = "*";
+          pandoc = "*";
+          forester = "*";
+          ocamlformat = "*";
         };
-      };
-    });
+        query = devPackagesQuery // { };
+        scope = on.buildOpamProject' { } ./. query;
+        overlay = final: prev: {
+          ${package} =
+            prev.${package}.overrideAttrs (_: { doNixSupport = false; });
+        };
+        scope' = scope.overrideScope' overlay;
+        main = scope'.${package};
+        devPackages = builtins.attrValues
+          (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope');
+      in {
+        legacyPackages = scope';
+
+        packages.default = main;
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ main ];
+          buildInputs = with pkgs;
+            devPackages ++ [
+              libev
+              pkg-config
+              openssl
+            ];
+        };
+      });
 }
