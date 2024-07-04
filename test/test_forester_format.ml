568b46aa752d1d671c7f5a0f90e912a2020bd397
Kento Okura <kentookura@gmail.com>
2024-07-04 13:16:24 +0200

    properly set up diff testing

--- test/test_forester_format.ml
+++ test/test_forester_format.ml
@@ -5,23 +5,21 @@ module Tty = Asai.Tty.Make (Forester_core.Reporter.Message)
 
 exception Foo
 
-let tree =
-  let code = Parse.parse_string {|
-\title{asfd}
-\p{
-  asdf
-}
-|} in
-  match code with
-  | Ok code -> Code.{ source_path = None; addr = Some "foo"; code }
-  | Error _ -> raise Foo
+let ( / ) = Eio.Path.( / )
 
 let () =
   let fatal diagnostics =
     Tty.display diagnostics;
     exit 1
   in
-  (* Eio_main.run @@ fun _ -> *)
-  Reporter.run ~emit:Tty.display ~fatal @@ fun () ->
+  Eio_main.run @@ fun env ->
+  Forester_core.Reporter.run ~emit:Tty.display ~fatal @@ fun () ->
+  let cwd = Eio.Stdenv.cwd env in
+  let tree =
+    let code = Parse.parse_file (cwd / "test.tree") in
+    match code with
+    | Ok code -> Code.{ source_path = None; addr = Some "foo"; code }
+    | Error _ -> raise Foo
+  in
   let syn = Expand.expand_tree Expand.UnitMap.empty tree |> snd in
   print_tree syn
